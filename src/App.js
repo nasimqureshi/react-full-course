944144f0a4b2952863a153e3c22cf612be2fada0
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-10 10:31:41 -0600

    commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+// let name = "Nasim Qureshi"
+function App() {
+ return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <h1>Hello Nasim</h1>
+        <p>Lorem50</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
